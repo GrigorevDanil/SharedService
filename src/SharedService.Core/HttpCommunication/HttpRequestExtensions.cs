da065ac44fd70beeda05c8ed1e69fa29b7e0ca25
GrigorevDanil <danilg467@gmail.com>
2025-12-30 23:59:12 +0300

    Add HttpCommunication

---
+++ src/SharedService.Core/HttpCommunication/HttpRequestExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
+namespace SharedService.Core.HttpCommunication;
+
+public static class HttpRequestExtensions
+{
+    extension(HttpClient client)
+    {
+        public async Task<HttpResponseMessage> GetAsync<TQuery>(
+            string url,
+            TQuery query,
+            CancellationToken cancellationToken = default)
+            where TQuery : class
+            => await client.GetAsync(GenerateUrlWithQuery(url, query), cancellationToken);
+
+        public async Task<HttpResponseMessage> DeleteAsync<TQuery>(
+            string url,
+            TQuery query,
+            CancellationToken cancellationToken = default)
+            where TQuery : class
+            => await client.DeleteAsync(GenerateUrlWithQuery(url, query), cancellationToken);
+    }
+
+    private static string GenerateUrlWithQuery<TQuery>(
+        string url,
+        TQuery query)
+        where TQuery : class
+    {
+        var parameters = new Dictionary<string, StringValues>();
+
+        foreach (PropertyInfo prop in query.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+        {
+            object? value = prop.GetValue(query, null);
+
+            string propName = JsonNamingPolicy.CamelCase.ConvertName(prop.Name);
+
+            if (value is IEnumerable enumerable and not string)
+            {
+                string?[] items = enumerable
+                    .OfType<object>()
+                    .Select(item => item.ToString())
+                    .ToArray();
+
+                if (items.Length > 0)
+                    parameters[propName] = new StringValues(items);
+            }
+            else
+            {
+                parameters[propName] = new StringValues(value?.ToString());
+            }
+        }
+
+        return QueryHelpers.AddQueryString(url, parameters);
+    }
+}
---
+++ src/SharedService.Core/HttpCommunication/HttpResponseMessageExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Json;
+using CSharpFunctionalExtensions;
+using SharedService.SharedKernel;
+
+namespace SharedService.Core.HttpCommunication;
+
+public static class HttpResponseMessageExtensions
+{
+    extension(HttpResponseMessage response)
+    {
+        public async Task<Result<TResponse?, Errors>> HandleResponseAsync<TResponse>(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                Envelope<TResponse>? data =
+                    await response.Content.ReadFromJsonAsync<Envelope<TResponse>>(cancellationToken);
+
+                if (!response.IsSuccessStatusCode)
+                    return data?.ErrorList ?? GeneralErrors.Failure("Unknown error");
+
+                if (data is null)
+                    return GeneralErrors.Failure("Error while reading response").ToErrors();
+
+                if (data.ErrorList is not null)
+                    return data.ErrorList;
+
+                return data.Result;
+            }
+            catch
+            {
+                return GeneralErrors.Failure("Error while reading response").ToErrors();
+            }
+        }
+
+        public async Task<UnitResult<Errors>> HandleResponseAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                Envelope? data = await response.Content.ReadFromJsonAsync<Envelope>(cancellationToken);
+
+                if (!response.IsSuccessStatusCode)
+                    return data?.ErrorList ?? GeneralErrors.Failure("Unknown error");
+
+                if (data is null)
+                    return GeneralErrors.Failure("Error while reading response").ToErrors();
+
+                if (data.ErrorList is not null)
+                    return data.ErrorList;
+
+                return UnitResult.Success<Errors>();
+            }
+            catch
+            {
+                return GeneralErrors.Failure("Error while reading response").ToErrors();
+            }
+        }
+    }
+}
